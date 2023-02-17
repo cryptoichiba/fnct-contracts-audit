9309bdd09cfec9dbbfe8938b54b3416928759160
rudo <rudoneko@gmail.com>
2023-02-17 14:24:54 +0900

    NatSpec for fixed interfaces

--- contracts/staking/fixed_interfaces/IFixedStaking.sol
+++ contracts/staking/fixed_interfaces/IFixedStaking.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-// Note: DO NOT EDIT THIS INTERFACE
+/**
+ * @dev Fixed interface of the Staking.
+ * @dev DO NOT EDIT THIS INTERFACE
+ */
 interface IFixedStaking {
     function calcUnlockable(address user) external view returns(uint256);
     function lockAndDelegate(uint256 amount, address validator) external;
