8c51702f6a4019be1b5ef3d461f441de73dd1cc9
rudo <rudoneko@gmail.com>
2023-02-17 18:39:14 +0900

    Update IRNG.sol

--- contracts/staking/interfaces/IRNG.sol
+++ contracts/staking/interfaces/IRNG.sol
@@ -11,6 +11,26 @@ struct RequestStatus {
 
 
 interface IRNG {
+    /// @notice Returns whether random number has been generated for "day"
+    /// @param day Day to check
+    /// @return True if a random number has been generated for "day"
+    function hadGeneratedNumber(uint day) external view returns (bool);
+
+    /// @notice Returns random number generated for "day"
+    /// @param day Day to check
+    /// @return randNumber The random number generated for "day" (will fail if number has not been generated)
+    function getRandomNumber(uint day) external view returns (uint256 randNumber);
+
+    /// @notice Returns number of days before request with no callback is abandoned
+    /// @return Number of days before request with no callback is abandoned
+    function abandonDaysAfterRequesting() external pure returns (uint);
+
+    /// @notice Request random number linked to key "day", where 0 <= randomNumber < maxNumber
+    /// @param day The key to request, which is a 0-based day index (can only request 1 number per day)
+    /// @param maxNumber Specifies the range of requested number (0 <= randomNumber < maxNumber)
+    /// @return requestId The unique request id received from Chainlink when making the request
+    function requestRandomWords(uint day, uint256 maxNumber) external returns (uint256 requestId);
+    
     /**
      * @dev Emitted when `requester` is granted permission to use RNG generator
      */
@@ -38,23 +58,4 @@ interface IRNG {
      */
     event LinkTokenWithdrawn(uint256 balance);
 
-    /// @notice Returns whether random number has been generated for "day"
-    /// @param day Day to check
-    /// @return True if a random number has been generated for "day"
-    function hadGeneratedNumber(uint day) external view returns (bool);
-
-    /// @notice Returns random number generated for "day"
-    /// @param day Day to check
-    /// @return randNumber The random number generated for "day" (will fail if number has not been generated)
-    function getRandomNumber(uint day) external view returns (uint256 randNumber);
-
-    /// @notice Returns number of days before request with no callback is abandoned
-    /// @return Number of days before request with no callback is abandoned
-    function abandonDaysAfterRequesting() external pure returns (uint);
-
-    /// @notice Request random number linked to key "day", where 0 <= randomNumber < maxNumber
-    /// @param day The key to request, which is a 0-based day index (can only request 1 number per day)
-    /// @param maxNumber Specifies the range of requested number (0 <= randomNumber < maxNumber)
-    /// @return requestId The unique request id received from Chainlink when making the request
-    function requestRandomWords(uint day, uint256 maxNumber) external returns (uint256 requestId);
 }
