de8ee0045ad4d05744836dd71c749f74f1b0a2f7
rudo <rudoneko@gmail.com>
2023-03-19 19:20:27 +0900

    Improve test suite for gas usage evaluation

--- test/performance_each_key_functions.js
+++ test/performance_each_key_functions.js
@@ -45,8 +45,18 @@ const deployFixture = async () => {
     };
 };
 
-describe("Performance: Pattern of increasing number of users", () => {
-    const num_users = 10;
+// Usage:
+//   remove .skip
+//   exec command: CI=true REPORT_GAS=1 npx hardhat test test/performance_each_key_functions.js
+//   eval gas usages, especially about key functions [submit/lockAndDelegate/unlock/claimReward/claimStakingCommission]
+//   change the numbers of the below constants and re-run command, then eval gas usage of key functions difference against the diff of each constants
+describe.skip("Performance: Pattern of increasing number of delegators/lock/unlock/submission/running days", () => {
+    const num_users = 1;
+    const call_lock_per_user = 1000;
+    const call_lock_per_other_user = 1;
+    const call_unlock_per_user = 1000;
+    const call_unlock_per_other_user = 1;
+    const submit_per_day = 1;
     const days = 3; // More than 2
 
     it("User call lockAndDelegate", async () => {
@@ -81,20 +91,56 @@ describe("Performance: Pattern of increasing number of users", () => {
 
         await TimeContract.setCurrentTimeIndex(0);
 
-        for (let i = 0; i < users.length; i++) {
-            await FNCToken.mint(users[i].address, 1);
-            await FNCToken.connect(users[i]).approve(VaultContract.address, 1);
-            await StakingContract.connect(users[i]).lockAndDelegate(1, sample(validators).address);
+        let validator = sample(validators).address;
+
+        // other's lock: lock call_unlock_per_other_user * call_lock_per_other_user
+        for (let i = 1; i < users.length; i++) {
+            await FNCToken.mint(users[i].address, call_lock_per_other_user * call_unlock_per_other_user);
+            await FNCToken.connect(users[i]).approve(VaultContract.address, call_lock_per_other_user * call_unlock_per_other_user);
+            for (let j = 0; j < call_lock_per_other_user; j++ ) {
+                await StakingContract.connect(users[i]).lockAndDelegate(call_unlock_per_other_user, validator);
+            }
+        }
+
+        // self lock: lock call_unlock_per_user * call_lock_per_user
+        await FNCToken.mint(users[0].address, call_lock_per_user * call_unlock_per_user);
+        await FNCToken.connect(users[0]).approve(VaultContract.address, call_lock_per_user * call_unlock_per_user);
+        for (let j = 0; j < call_lock_per_user; j++ ) {
+            await StakingContract.connect(users[0]).lockAndDelegate(call_unlock_per_user, validator);
         }
 
         await TimeContract.setCurrentTimeIndex(181);
 
-        for (let i = 0; i < users.length; i++) {
-            await StakingContract.connect(users[i]).unlock(1);
+        // other's unlock: call_lock_per_other_user * call_unlock_per_other_user
+        for (let i = 1; i < users.length; i++) {
+            for (let j = 0; j < call_unlock_per_other_user; j++ ) {
+                await StakingContract.connect(users[i]).unlock(call_lock_per_other_user);
+            }
+        }
+
+        // self unlock: call_lock_per_user * call_unlock_per_user
+        for (let j = 0; j < call_unlock_per_user; j++ ) {
+            await StakingContract.connect(users[0]).unlock(call_lock_per_user);
+        }
+
+        // other's additional lock: 1 * call_lock_per_other_user
+        for (let i = 1; i < users.length; i++) {
+            await FNCToken.mint(users[i].address, call_lock_per_other_user);
+            await FNCToken.connect(users[i]).approve(VaultContract.address, call_lock_per_other_user);
+            for (let j = 1; j < call_lock_per_other_user; j++ ) {
+                await StakingContract.connect(users[i]).lockAndDelegate(1, validator);
+            }
+        }
+
+        // self additional lock: 1 * call_lock_per_user
+        await FNCToken.mint(users[0].address, call_lock_per_user);
+        await FNCToken.connect(users[0]).approve(VaultContract.address, call_lock_per_user);
+        for (let j = 1; j < call_lock_per_user; j++ ) {
+            await StakingContract.connect(users[0]).lockAndDelegate(1, validator);
         }
     })
 
-    it("User call getStakingReward", async () => {
+    it.only("User call getStakingReward", async () => {
         const {
             owner,
             suppliyer,
@@ -117,10 +163,38 @@ describe("Performance: Pattern of increasing number of users", () => {
         await FNCToken.connect(owner).approve(RewardContract.address, allocateSize)
         await RewardContract.connect(owner).supplyStakingPool(1, allocateSize);
 
-        for ( let i = 0; i < users.length; i++ ) {
-            await FNCToken.mint(users[i].address, 1);
-            await FNCToken.connect(users[i]).approve(VaultContract.address, 1);
-            await StakingContract.connect(users[i]).lockAndDelegate(1, validators[0].address);
+        let validator = validators[0].address;
+
+        // other's lock: call_unlock_per_other_user * call_lock_per_other_user + 1
+        for (let i = 1; i < users.length; i++) {
+            await FNCToken.mint(users[i].address, call_lock_per_other_user * call_unlock_per_other_user + 1);
+            await FNCToken.connect(users[i]).approve(VaultContract.address, call_lock_per_other_user * call_unlock_per_other_user + 1);
+            await StakingContract.connect(users[i]).lockAndDelegate(1, validator);
+            for (let j = 0; j < call_lock_per_other_user; j++ ) {
+                await StakingContract.connect(users[i]).lockAndDelegate(call_unlock_per_other_user, validator);
+            }
+        }
+
+        // self lock: call_unlock_per_user * call_lock_per_user + 1
+        await FNCToken.mint(users[0].address, call_lock_per_user * call_unlock_per_user + 1);
+        await FNCToken.connect(users[0]).approve(VaultContract.address, call_lock_per_user * call_unlock_per_user + 1);
+        await StakingContract.connect(users[0]).lockAndDelegate(1, validator);
+        for (let j = 0; j < call_lock_per_user; j++ ) {
+            await StakingContract.connect(users[0]).lockAndDelegate(call_unlock_per_user, validator);
+        }
+
+        await TimeContract.setCurrentTimeIndex(181);
+
+        // other's unlock call_lock_per_user * call_unlock_per_user
+        for (let i = 1; i < users.length; i++) {
+            for (let j = 0; j < call_unlock_per_other_user; j++ ) {
+                await StakingContract.connect(users[i]).unlock(call_lock_per_other_user);
+            }
+        }
+
+        // self unlock call_lock_per_user * call_unlock_per_user
+        for (let j = 0; j < call_unlock_per_user; j++ ) {
+            await StakingContract.connect(users[0]).unlock(call_lock_per_user);
         }
 
         for ( let day = 0; day < days; day++ ) {
@@ -129,32 +203,46 @@ describe("Performance: Pattern of increasing number of users", () => {
             const file1 = web3.utils.hexToBytes("0xabcdabcd" + String(day+1));
             const file2 = web3.utils.hexToBytes("0xabcdabcd" + String(day+2));
 
-            await TimeContract.setCurrentTimeIndex(day);
+            await TimeContract.setCurrentTimeIndex(181 + day);
 
             if ( day == 0 ) {
-                await LogFileHash.connect(validators[0]).submit(validators[0].address, day, file0, file1);
-                await LogFileHash.connect(validators[1]).submit(validators[1].address, day, file0, file1);
-                await LogFileHash.connect(validators[2]).submit(validators[2].address, day, file0, file1);
+                console.log(day);
+                for ( let i = 0; i < submit_per_day; i++ ) {
+                    await LogFileHash.connect(validators[0]).submit(validators[0].address, day, file0, file1);
+                    await LogFileHash.connect(validators[1]).submit(validators[1].address, day, file0, file1);
+                    await LogFileHash.connect(validators[2]).submit(validators[2].address, day, file0, file1);
+                }
             } else {
+                console.log(day);
+                // first submission
                 await LogFileHash.connect(validators[0]).submit(validators[0].address, day - 1, file0, file1);
                 await LogFileHash.connect(validators[1]).submit(validators[1].address, day, file1, file2);
                 await LogFileHash.connect(validators[2]).submit(validators[2].address, day, file1, file2);
+
+                // after first submission
+                for ( let i = 0; i < submit_per_day - 1; i++ ) {
+                    await LogFileHash.connect(validators[0]).submit(validators[0].address, day, file1, file2);
+                    await LogFileHash.connect(validators[1]).submit(validators[1].address, day, file1, file2);
+                    await LogFileHash.connect(validators[2]).submit(validators[2].address, day, file1, file2);
+                }
 
                 // Gen previous day's seed (Request ID is (day-1)+1 ( VRFCoordinatorV2Mock.sol assigns IDs [1,2,3...] )
                 await ChainlinkCoordinator.connect(owner).fulfillRandomWordsWithOverride(
                     BigNumber.from(day), ChainlinkWrapper.address, [0])
 
-                await LogFileHash.getMajority(day - 1);
+                // await LogFileHash.getMajority(day - 1);
             }
         }
 
-        for ( let i = 0; i < 1; i++ ) {
-            for ( let j = 0; j < 5; j++ ) {
-                await RewardContract.connect(users[i]).claimStakingReward();
-                await RewardContract.connect(validators[0]).claimStakingCommission(validators[0].address);
-                console.log("FNCT Balance / user:       " + ethers.utils.formatEther(await FNCToken.balanceOf(users[i].address)));
-                console.log("FNCT Balance / validator:  " + ethers.utils.formatEther(await FNCToken.balanceOf(validators[0].address)));
-            }
+        // first delegator / validator
+        // claim function will provide 45 days reward/commission at once
+        // the test contains 180 + days scenario
+        // therefore, (180 + days) / 45 times claim required for receiving whole rewards/commission
+        for ( let j = 0; j < (180 + days) / 45 + 1; j++ ) {
+            await RewardContract.connect(users[0]).claimStakingReward();
+            await RewardContract.connect(validators[0]).claimStakingCommission(validators[0].address);
+            console.log("FNCT Balance / user:       " + ethers.utils.formatEther(await FNCToken.balanceOf(users[0].address)));
+            console.log("FNCT Balance / validator:  " + ethers.utils.formatEther(await FNCToken.balanceOf(validators[0].address)));
         }
     })
 });
